28c1ce06bebdde218593ccd2e6d82abe3dc60620
Deepu Kumar <Deepujee2020@gmail.com>
2024-01-17 01:20:01 +0530

    Added category list markups and styles

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import styles from "./categoryList.module.css";
-
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = async () => {
   return (
     <div className={styles.container}>
-      CategoryList
+      <h1 className={styles.title}>
+        Popular Category
+      </h1>
+      <div className={styles.categories}>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`} >
+          <Image src="/style.png" alt="category_image" width={32} height={32} className={styles.images} />
+          Style
+        </Link>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.fashion}`} >
+          <Image src="/fashion.png" alt="category_image" width={32} height={32} className={styles.images} />
+          Fashion
+        </Link>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.food}`} >
+          <Image src="/food.png" alt="category_image" width={32} height={32} className={styles.images} />
+          Food
+        </Link>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.travel}`} >
+          <Image src="/travel.png" alt="category_image" width={32} height={32} />
+          Travel
+        </Link>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.culture}`} >
+          <Image src="/culture.png" alt="category_image" width={32} height={32} className={styles.images} />
+          Culture
+        </Link>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.coding}`} >
+          <Image src="/coding.png" alt="category_image" width={32} height={32} className={styles.images} />
+          Coding
+        </Link>
+      </div>
     </div>
   );
 };
