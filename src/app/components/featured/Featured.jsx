410dabb5ff4ed96377ff5b817a59d2f6616e0758
Deepu Kumar <Deepujee2020@gmail.com>
2024-01-17 01:20:29 +0530

    Added featured component

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 
 const Featured = () => {
   return (
-    <div className={styles.container}>
-      Featured
+    <div>
+      <h1 className={styles.title}>
+        <b>Hey, Deepu Kumar here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="post_image" fill className={styles.images} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, odit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo inventore, ullam provident odit necessitatibus aspernatur omnis dignissimos. Expedita totam quos repellat, officia inventore ab sapiente.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
   );
 };
